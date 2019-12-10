d2990ce948c714c64742ac23a833cd46392858c4
gmsy <gmsy@gmsy.gmsy>
2019-12-10 11:08:26 -0500

    Config script files

--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -1,30 +1,33 @@
-import React, { Component, Fragment } from 'react';
-import actions from '../../services/index'
+import React, { Component, Fragment } from "react";
+import actions from "../../services/index";
 
 class LogIn extends Component {
+  state = {};
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
-    state = {
-
-    } 
-    handleChange = e => this.setState({[e.target.name]: e.target.value})
-
-    handleSubmit = async e => {
-        e.preventDefault()
-        let user = await actions.logIn(this.state);
-        this.props.setUser({...user.data})  
-    }
-    render() {
-        return (
-            <Fragment>
-                <h2>LogIn</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" onChange={this.handleChange} />
-                    <input name="password" type="password" onChange={this.handleChange} />
-                    <input type="submit" value="Log In"/>
-                </form>
-            </Fragment>
-        );
-    }
+  handleSubmit = async e => {
+    e.preventDefault();
+    let user = await actions.logIn(this.state);
+    this.props.setUser({ ...user.data });
+  };
+  render() {
+    return (
+      <Fragment>
+        <h2>LogIn</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input name="email" type="email" onChange={this.handleChange} />{" "}
+          <br />
+          <input
+            name="password"
+            type="password"
+            onChange={this.handleChange}
+          />{" "}
+          <br />
+          <input type="submit" value="Log In" />
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default LogIn;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
-import actions from '../../services/index'
-
+import React, { Component } from "react";
+import actions from "../../services/index";
 
 class Home extends Component {
   async componentDidMount() {
     //actions.test()
   }
   render() {
-    return (
-      <div>
-        
-      </div>
-    );
+    return <div>thats the home</div>;
   }
 }
 
