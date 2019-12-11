a480bfa100846ef94a34f625392c77e1e889fb48
gmsy <gmsy@gmsy.gmsy>
2019-12-11 10:47:29 -0500

    Update donate.jsx

--- src/components/donate/donate.jsx
+++ src/components/donate/donate.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import axios from "axios";
 
 export default class Donate extends React.Component {
+  componentDidMount = async () => {
+    const theMessage = await axios.get("http://localhost:5000/donate");
+    console.log(theMessage);
+  };
   render() {
     return (
       <React.Fragment>
