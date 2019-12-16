45cf1d3514df49d4d9322370e35b4130e257c766
robbiesanchez <rfsanchez004@gmail.com>
2019-12-16 11:41:36 -0500

    rendering Funds and Transactions on User profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import {Link} from "react-router-dom"
 
 class Profile extends Component {
+  state = {
+    myStuff: null
+  }
   async componentDidMount() {
     // if (!this.props.user.email) {
     //   this.props.history.push("/log-in");
@@ -11,17 +15,44 @@ class Profile extends Component {
       withCredentials: true
     });
     console.log(myStuff);
-    this.setState({});
+    this.setState({myStuff})
+
+  }
+
+  showMyFunds = () => {
+    return (this.state.myStuff.data.theFunds.map((res,i)=> (
+      <div key={i}>
+       <Link to={"/fund/" + res._id}>{res.title}</Link> 
+      </div>
+    )))
+  }
+
+  showMyDonations = () => {
+    return (this.state.myStuff.data.theTransactions.map((res,i)=> (
+      <div key={i}>
+        {res.amount}
+      </div>
+    )))
   }
 
   render() {
+    console.log(this.state)
+    if (this.state.myStuff)
     return (
       <div>
         Profile <br />
         {/* Welcome {this.props.user.name} <br />
         Email: {this.props.user.email} */}
+        My Funds: {this.showMyFunds()}
+        My Donations: {this.showMyDonations()}
       </div>
     );
+    else
+    {
+      return(
+      <div>Loading</div>
+    )
+    }
   }
 }
 
