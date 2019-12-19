d50e469bb06ffed3da82fa58f539d0b2f338e0e7
robbiesanchez <rfsanchez004@gmail.com>
2019-12-19 16:35:19 -0500

    Updates

---
+++ precache-manifest.9af08844d66c3b6e98a49e03042bb1ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f344ad65d4f4d4dd67f9eb634ca98d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca60bb358bd79b3a969b",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "be213a8398a1716d953b",
+    "url": "/static/css/main.698a93c1.chunk.css"
+  },
+  {
+    "revision": "ca60bb358bd79b3a969b",
+    "url": "/static/js/2.51540d58.chunk.js"
+  },
+  {
+    "revision": "be213a8398a1716d953b",
+    "url": "/static/js/main.fa2ed185.chunk.js"
+  },
+  {
+    "revision": "80f95f4d0fbbd034142a",
+    "url": "/static/js/runtime-main.6b303a61.js"
+  },
+  {
+    "revision": "be561b285d36afa4618caf4a7ceeeb90",
+    "url": "/static/media/bgv.be561b28.mp4"
+  },
+  {
+    "revision": "a3e19abd66a3022f9c1ff6b9da9de92b",
+    "url": "/static/media/leaf1.a3e19abd.png"
+  }
+]);
