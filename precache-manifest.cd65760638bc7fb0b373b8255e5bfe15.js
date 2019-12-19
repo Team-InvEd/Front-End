70cc6ada18d3f5a88b948353a05432f7d8aac0d3
robbiesanchez <rfsanchez004@gmail.com>
2019-12-19 16:53:45 -0500

    Updates

---
+++ precache-manifest.cd65760638bc7fb0b373b8255e5bfe15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d4ea6751b426d89340f5aa1701b5d2",
+    "url": "/Front-End/index.html"
+  },
+  {
+    "revision": "70023bc4b18a6a0629ab",
+    "url": "/Front-End/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4ba324d709e931103876",
+    "url": "/Front-End/static/css/main.698a93c1.chunk.css"
+  },
+  {
+    "revision": "70023bc4b18a6a0629ab",
+    "url": "/Front-End/static/js/2.97e80963.chunk.js"
+  },
+  {
+    "revision": "4ba324d709e931103876",
+    "url": "/Front-End/static/js/main.411ba1f0.chunk.js"
+  },
+  {
+    "revision": "18cb053e9bb46195c1ea",
+    "url": "/Front-End/static/js/runtime-main.371a6e9d.js"
+  },
+  {
+    "revision": "be561b285d36afa4618caf4a7ceeeb90",
+    "url": "/Front-End/static/media/bgv.be561b28.mp4"
+  },
+  {
+    "revision": "a3e19abd66a3022f9c1ff6b9da9de92b",
+    "url": "/Front-End/static/media/leaf1.a3e19abd.png"
+  }
+]);
